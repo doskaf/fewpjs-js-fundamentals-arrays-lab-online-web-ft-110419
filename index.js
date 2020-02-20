c6fe159a3a991bd349df9062f6ee5fdb558aae16
Dominique Skaf <doskaf@yahoo.com>
2020-02-20 22:22:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ let removeLast = ["Milo", "Otis", "Garfield"];
 let removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
