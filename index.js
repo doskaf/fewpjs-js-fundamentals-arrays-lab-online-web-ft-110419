550801149c640320ff971739a6422bcef5794d39
Dominique Skaf <doskaf@yahoo.com>
2020-02-20 22:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let append = ["Milo", "Otis", "Garfield"];
 let prepend = ["Milo", "Otis", "Garfield"];
 let removeLast = ["Milo", "Otis", "Garfield"];
 let removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
